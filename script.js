f92624579100b7db3e09f502fcad05992e124c2a
unknown <dequanchambers94@gmail.com>
2019-10-30 09:05:44 -0400

    update part2

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ class AddressBook {
     add(name, email, phone, relation) {
         this.contacts.push(new Contact(name, email, phone, relation));
     }
+
+    // findByName(name) {
+    //     function findName() {
+    //         return this.contacts;
+    //     }
+    // }
 }
 
 class Contact {
@@ -20,7 +26,29 @@ function print(info) {
     console.log(info);
 }
 
+// function display(){
+// AddressBook.contacts.forEach((contact,index)=>{
+//     const newEntry = document.createElement("div")
+//     newEntry.classList.add("box")
+//     newEntry.innerHTML=
+//     <p>name</p>
+//     <p>email</p>
+//     <p>phone</p>
+//     <p>relation</p>
+//     document.querySelector("#container").appendChild(newEntry)
+
+// })
+// }
+// const form = document.querySelector("form")
+// form.addEventListener("submit",addContact)
+// function addContact(e){
+// e.preventDefault();
+// const formData= new formData(form);
+
+// }
+
 let book = new AddressBook();
 book.add("charm", "charmhorn@gmail.com", 3134453045);
 book.add("dequan", "example@gmail.com", 3134525525);
+
 print(book);
